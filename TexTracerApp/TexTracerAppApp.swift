ba81cab76110ef9b61938383f7f93cfbd88c24da
Lisa Aldera <alderalisa1@gmail.com>
2023-12-01 16:29:44 +0100

    Initial Commit

---
+++ TexTracerApp/TexTracerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TexTracerAppApp.swift
+//  TexTracerApp
+//
+//  Created by opendag on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TexTracerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

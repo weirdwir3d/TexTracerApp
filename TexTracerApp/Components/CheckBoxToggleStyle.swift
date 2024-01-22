b75294647a3b57183187a2a34dfddb44c689ede1
Lisa Aldera <alderalisa1@gmail.com>
2024-01-22 17:58:28 +0100

    edited CheckBoxToggle thingie

--- TexTracerApp/Components/CheckBoxToggleStyle.swift
+++ TexTracerApp/Components/CheckBoxToggleStyle.swift
@@ -4,22 +4,20 @@ import SwiftUI
 struct CheckboxToggleStyle: ToggleStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
- 
             RoundedRectangle(cornerRadius: 5.0)
                 .stroke(lineWidth: 2)
                 .frame(width: 25, height: 25)
                 .cornerRadius(5.0)
                 .overlay {
-                    Image(systemName: configuration.isOn ? "checkmark" : "")
+                    Image(systemName: configuration.isOn ? "checkmark" : "square")
                 }
                 .onTapGesture {
                     withAnimation(.spring()) {
                         configuration.isOn.toggle()
                     }
                 }
- 
             configuration.label
- 
         }
     }
 }
+
---
+++ TexTracerApp/Extensions/View.swift
@@ -0,0 +1,8 @@
+//
+//  View.swift
+//  TexTracerApp
+//
+//  Created by opendag on 22/01/2024.
+//
+
+import Foundation
